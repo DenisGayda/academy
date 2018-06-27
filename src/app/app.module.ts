0d8412fa4f5e5058740971f7831d518dae8ae762
Yuliia Lepska <iuliia.chumachenko@gmail.com>
2018-06-27 18:19:18 +0300

    [TFA-5] authorisation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireAuthModule } from 'angularfire2/auth'
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from '../environments/environment';
 import { RoutingModule } from './routing.module';
 import { FooterModule } from './footer/footer.module';
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl} from '@angular/forms';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { auth } from 'firebase';
 
@@ -7,15 +8,45 @@ import { auth } from 'firebase';
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  public txtEmail = '';
+  public txtPassword = '';
+  public emailSearcher: FormControl;
+  public passwordSearcher: FormControl;
   constructor(public afAuth: AngularFireAuth) {
   }
-
-  public login(): void {
+  ngOnInit(): void {
+    this.emailSearcher = new FormControl('');
+    this.passwordSearcher = new FormControl('');
+    this.emailSearcher.valueChanges.subscribe(value => {
+      this.txtEmail = value;
+    });
+    this.passwordSearcher.valueChanges.subscribe(value => {
+      this.txtPassword = value;
+    });
+  }
+  public loginWithGoogle(): void {
     this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+
+  public loginWithGithub(): void {
+    this.afAuth.auth.signInWithPopup(new auth.GithubAuthProvider());
   }
 
   public logout(): void {
     this.afAuth.auth.signOut();
+  }
+
+  public login(): void {
+    const promise = this.afAuth.auth.signInWithEmailAndPassword(this.txtEmail, this.txtPassword);
+    promise.catch(e => {
+      throw new Error(e);
+    });
+  }
+  public signup(): void {
+    const promise = this.afAuth.auth.createUserWithEmailAndPassword(this.txtEmail, this.txtPassword);
+    promise.catch(e => {
+      throw new Error(e);
+    });
   }
 }
