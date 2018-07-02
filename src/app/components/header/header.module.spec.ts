b1c706ded6661e6c32f6ad2ed8c5e041da386bc2
VladGaryachiy <32650566+VladGaryachiy@users.noreply.github.com>
2018-07-02 09:13:35 +0300

    new change

--- src/app/components/header/header.module.spec.ts
+++ src/app/components/header/header.module.spec.ts
@@ -1,9 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
-
 import { RouterModule } from '@angular/router';
-
 import {RouterTestingModule} from '@angular/router/testing';
 
 describe('HeaderComponent', () => {
