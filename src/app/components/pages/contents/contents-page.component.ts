99c131eb4c0e92b3f6513abaa8b86a63e878a1cb
mihailnetkachev <mihailnetkachev.8@gmail.com>
2018-07-02 16:16:01 +0300

    [TFA-22] Refactoring the project structure.

--- src/app/components/pages/contents/contents-page.component.ts
+++ src/app/components/pages/contents/contents-page.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contents-page',
   templateUrl: './contents-page.component.html',
-  styleUrls: ['./contents-page.component.css'],
+  styleUrls: ['./contents-page.component.scss'],
 })
 export class ContentsPageComponent {}
