b21bf04d15016db68f52be9ad30f0ede705cb437
mihailnetkachev <mihailnetkachev.8@gmail.com>
2018-07-03 17:25:23 +0300

    [TFA-25] Example-exercise was created.

--- src/app/pages/exercises/exercises-page.component.spec.ts
+++ src/app/pages/exercises/exercises-page.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { FormsModule } from '@angular/forms';
+import { MonacoEditorModule } from 'ngx-monaco-editor';
 import { ExercisesPageComponent } from './exercises-page.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 describe('ExercisesPageComponent', () => {
   let component: ExercisesPageComponent;
@@ -8,6 +12,12 @@ describe('ExercisesPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        MonacoEditorModule.forRoot(),
+        MatCardModule,
+        MatButtonModule,
+      ],
       declarations: [ ExercisesPageComponent ],
     })
     .compileComponents();
--- src/app/pages/exercises/exercises-page.component.ts
+++ src/app/pages/exercises/exercises-page.component.ts
@@ -5,4 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './exercises-page.component.html',
   styleUrls: ['./exercises-page.component.scss'],
 })
-export class ExercisesPageComponent { }
+export class ExercisesPageComponent {
+
+  options = {
+    theme: 'vs-dark',
+    language: 'javascript',
+  };
+  exercise = {
+    number: 1,
+    text: 'Please, create modal window with "Hello"',
+    answer: 'alert("Hello");',
+  };
+  code = ``;
+
+  checkAnswer(): void {
+    const result = (this.exercise.answer === this.code) ? 'Awesome! Congratulations!' : 'Try again.';
+    alert(result);
+  }
+
+}
