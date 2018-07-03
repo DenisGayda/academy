b40b123389fc18078d5004427a4df9a71abf5924
mihailnetkachev <mihailnetkachev.8@gmail.com>
2018-07-03 17:35:07 +0300

    [TFA-25] Exercise was updated.

--- src/app/pages/exercises/exercises-page.component.ts
+++ src/app/pages/exercises/exercises-page.component.ts
@@ -13,8 +13,9 @@ export class ExercisesPageComponent {
   };
   exercise = {
     number: 1,
-    text: 'Please, create modal window with "Hello"',
-    answer: 'alert("Hello");',
+    title: 'Create a date',
+    text: 'Create a Date object for the date: February 20, 2012, 3 hours 12 minutes. Use "var" and name "myDate".',
+    answer: 'var myDate = new Date(2012, 1, 20, 3, 12);',
   };
   code = ``;
 
