098f0176ca813178500fba4efe5f7930f4e30770
Rostislav Kozar <argulusyn@gmail.com>
2018-11-27 11:47:20 +0200

    Refactor name of the User class

--- src/app/model/user-profile.class.ts
+++ src/app/model/user-profile.class.ts
@@ -1,5 +1,5 @@
 import { IUser } from './user-profile.interface';
 
-export class User implements IUser {
+export class UserInDB implements IUser {
     constructor(public name: string, public email: string, public adress: string, public phone: string) {}
 }
