e62614453ae15ee68ca36a9292fba5a52c2da16a
Rostislav Kozar <argulusyn@gmail.com>
2018-11-27 11:44:13 +0200

    Add mehod for waiting until user is loaded from DB

--- src/app/pages/profile/profile-page.component.ts
+++ src/app/pages/profile/profile-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { FirebaseService } from '../../services/firebase-service/firebase.service';
 import { IUser } from '../../model/user-profile.interface';
-import { User } from '../../model/user-profile.class';
+import { UserInDB } from '../../model/user-profile.class';
 
 @Component({
     selector: 'app-profile-page',
@@ -11,46 +11,27 @@ import { User } from '../../model/user-profile.class';
 })
 export class ProfilePageComponent implements OnInit {
     public userForm: FormGroup;
-    public userName: string;
-    public userEmail: string;
-    public userAdress: string;
-    public userPhone: string;
-    public user;
-    public count = 0;
     public hiddenForm = false;
     public hiddenSuccessBlock: boolean;
+    public userFromDB;
 
     constructor(private firebaseService: FirebaseService) {
     }
 
     ngOnInit(): void {
-        this.firebaseService.dataInUsers.subscribe(data => {
-            data.map((item, i) => {
-                this.count = i + 2;
-            });
-        });
-        this.user = this.firebaseService.user;
         this.createForm();
-    }
-
-    public generateIdForUser(): string {
-        let id = '';
-        id += this.count;
-
-        return id;
     }
 
     public createForm(): void {
         this.userForm = new FormGroup({
             userName: new FormControl(null, null),
-            userEmail: new FormControl(null,
-                null),
-            userAdress: new FormControl('', Validators.required),
-            userPhone: new FormControl('+3', Validators.required),
+            userEmail: new FormControl(null, null),
+            userAdress: new FormControl(null, Validators.required),
+            userPhone: new FormControl(null, Validators.required),
         });
     }
 
-    public showSuccessBlock(): void {
+    private showSuccessBlock(): void {
         const time = 3000;
         const component = this;
         setTimeout(() => {
@@ -58,15 +39,31 @@ export class ProfilePageComponent implements OnInit {
         }, time);
     }
 
-    public createUserData(name: string, email: string): void {
+    public createUserData(): void {
         this.hiddenSuccessBlock = false;
-        this.userName = this.userForm.value.userName;
-        this.userEmail = this.userForm.value.userEmail;
-        this.userAdress = this.userForm.value.userAdress;
-        this.userPhone = this.userForm.value.userPhone;
-        const userData: IUser = new User(name, email, this.userAdress, this.userPhone);
-        this.firebaseService.setDataInDatabase('/Users', this.generateIdForUser(), userData);
-        this.hiddenForm = !this.hiddenForm;
+
+        const name = this.userForm.value.userName;
+        const email = this.firebaseService.userEmail;
+        const address = this.userForm.value.userAdress;
+        const phone = this.userForm.value.userPhone;
+        const newInfo: IUser = new UserInDB(name, email, address, phone);
+
+        this.firebaseService.setDataInDatabase('/Users', this.firebaseService.userId, newInfo);
+
         this.showSuccessBlock();
+    }
+
+    public isReady(): boolean {
+        if (this.userFromDB) {
+            return true;
+        }
+
+        if (this.firebaseService.userId) {
+            this.userFromDB = this.firebaseService.getCurrentUserFromDB();
+
+            return true;
+        }
+
+        return false;
     }
 }
